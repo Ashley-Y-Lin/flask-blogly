b1455836a6358ffa7c9d308a8fd00b7a8409bb71
Ashley Lin <ashleyylin11@gmail.com>
2023-04-26 12:38:18 -0700

    add html and connect database to flask

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 import os
 
 from flask import Flask
-from models import connect_db
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-    "DATABASE_URL", 'postgresql:///blogly')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL", "postgresql:///blogly"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
 
 connect_db(app)
